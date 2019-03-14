fabc8ae4e379bdec732aa7a2e3f5fd1b03d59cd6
Teo Cardoso <aec.gabrielsantos@gmail.com>
2019-03-14 14:20:57 -0300

    Rotate Image

--- edit_img_2.py
+++ edit_img_2.py
@@ -13,9 +13,16 @@ class Rotate_img():
         cv2.imshow("BIROLIRO", self.img)
 
     def rotate_img(self):
-
-        image_center = tuple(np.array(self.img.shape))
         
+        rows,cols,_ = self.img.shape
+        image_center = ((rows/2,cols/2))
+        rot_mat = cv2.getRotationMatrix2D(image_center, 90, 1)
+        self.result = cv2.warpAffine(self.img, rot_mat,(rows,cols), flags=cv2.INTER_LINEAR)
+        cv2.imshow('sapoha',self.result)
+    
+    def save_img(self):
+
+        cv2.imwrite("/home/teo/Downloads/taok_rot.jpg",self.result)
 
 if __name__ == "__main__":
 
@@ -24,6 +31,7 @@ if __name__ == "__main__":
     while True:
         run.show_img()
         run.rotate_img()
+        run.save_img()
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         break
